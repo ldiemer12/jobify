45981237e63b62a509817b20b59cfab92b0ab598
PatrickChewing8 <logandiemer@gmail.com>
2022-07-27 23:50:40 -0400

    Navbar

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -13,6 +13,8 @@ import {
 	SETUP_USER_BEGIN,
 	SETUP_USER_SUCCESS,
 	SETUP_USER_ERROR,
+	TOGGLE_SIDEBAR,
+	LOGOUT_USER,
 } from './actions';
 
 const token = localStorage.getItem('token ');
@@ -28,6 +30,7 @@ const initialState = {
 	token: null,
 	userLocation: userLocation || '',
 	jobLocation: userLocation || '',
+	showSidebar: false,
 };
 
 const AppContext = React.createContext();
@@ -119,9 +122,26 @@ const AppProvider = ({ children }) => {
 		clearAlert();
 	};
 
+	const toggleSidebar = () => {
+		dispatch({ type: TOGGLE_SIDEBAR });
+	};
+
+	const logoutUser = () => {
+		dispatch({ type: LOGOUT_USER });
+		removeUserFromLocalStorage();
+	};
+
 	return (
 		<AppContext.Provider
-			value={{ ...state, displayAlert, registerUser, loginUser, setupUser }}
+			value={{
+				...state,
+				displayAlert,
+				registerUser,
+				loginUser,
+				setupUser,
+				toggleSidebar,
+				logoutUser,
+			}}
 		>
 			{children}
 		</AppContext.Provider>
